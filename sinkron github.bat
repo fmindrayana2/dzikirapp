9c8111404b8161076ef0dd048e8714f8e8240c96
fmindrayana2 <fmindrayana2@gmail.com>
2025-10-18 08:06:03 +0700

    2025-10-18

--- sinkron github.bat
+++ sinkron github.bat
@@ -2,6 +2,6 @@
 set datestr=%date:~10,4%-%date:~4,2%-%date:~7,2%
 git add .
 git commit -m "%datestr%"
-git push origin master
+git push origin main
 title alhamdulillah DONE bisa di close windownya
 pause
